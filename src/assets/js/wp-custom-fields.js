62607650b5265ed07be9f562007ee8f87767e052
Michiel <michielsite@hotmail.com>
2018-08-02 12:51:15 +0200

    Fixed a bug when no datepicker is present, lowered the bottom margin for the fields

--- src/assets/js/wp-custom-fields.js
+++ src/assets/js/wp-custom-fields.js
@@ -111,34 +111,35 @@ module.exports.init = function(framework) {
  */
 module.exports.init = function(framework) {
 
-    var config = {
-            altFormat: 'F j, Y',
-            altInput: true,
-            dateFormat: 'U',
-            time_24hr: true,
-            wrap: true
-        },
-        datePicker = jQuery(framework).find('.wp-custom-fields-datepicker'),
-        propertyName,
-        propertyValue;
+    if( jQuery.fn.flatpickr ) {
 
-    // Grab our custom properties. For a description of these properties, see the datepicker.php file in the fields folder.
-    ['enable-time', 'alt-format', 'date-format', 'locale', 'max-date', 'min-date', 'mode', 'no-calendar', 'week-numbers'].forEach( function(value) {
+        var config = {
+                altFormat: 'F j, Y',
+                altInput: true,
+                dateFormat: 'U',
+                time_24hr: true,
+                wrap: true
+            },
+            datePicker = jQuery(framework).find('.wp-custom-fields-datepicker'),
+            propertyName,
+            propertyValue;
 
-        propertyValue = jQuery(datePicker).data(value);
+        // Grab our custom properties. For a description of these properties, see the datepicker.php file in the fields folder.
+        ['enable-time', 'alt-format', 'date-format', 'locale', 'max-date', 'min-date', 'mode', 'no-calendar', 'week-numbers'].forEach( function(value) {
 
-        if( propertyValue ) {
-            propertyName = value.replace( /-([a-z])/g, function (g) { return g[1].toUpperCase(); } );
-            config[propertyName] = propertyValue;
-        }
+            propertyValue = jQuery(datePicker).data(value);
 
-    });
+            if( propertyValue ) {
+                propertyName = value.replace( /-([a-z])/g, function (g) { return g[1].toUpperCase(); } );
+                config[propertyName] = propertyValue;
+            }
 
-    
-    console.log(config);
+        });
 
-    // Initializes the datepicker
-    jQuery(datePicker).flatpickr(config);
+        // Initializes the datepicker
+        jQuery(datePicker).flatpickr(config);
+
+    }
 
 }
 },{}],5:[function(require,module,exports){
