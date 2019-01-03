a7fc14360a9f25a6f5cb7e1a12d42c5a0c3d7320
Michiel <michielsite@hotmail.com>
2019-01-03 15:27:35 +0100

    Added the readonly support for input fields

--- src/fields/input.php
+++ src/fields/input.php
@@ -14,20 +14,20 @@ class Input implements Field {
     public static function render($field = array()) {
         
         $attributes  = '';
-        $class       = isset($field['class']) && $field['class'] ? $field['class'] : 'regular-text';
-        $placeholder = isset($field['placeholder']) && $field['placeholder'] ? ' placeholder="' . $field['placeholder'] . '"' : '';
-        $type        = isset($field['subtype']) && $field['subtype'] ? $field['subtype'] : 'text';
+        $class       = isset($field['class']) && $field['class'] ? esc_attr($field['class']) : 'regular-text';
+        $placeholder = isset($field['placeholder']) && $field['placeholder'] ? ' placeholder="' . esc_attr($field['placeholder']) . '"' : '';
+        $type        = isset($field['subtype']) && $field['subtype'] ? esc_attr($field['subtype']) : 'text';
 
-        foreach( array('min', 'max', 'step') as $attribute ) {
+        foreach( array('min', 'max', 'readonly', 'step') as $attribute ) {
             if( isset($field[$attribute]) && $field[$attribute] !== '' ) {
-                $attributes .= ' ' . $attribute . '="' . $field[$attribute] . '"';  
+                $attributes .= ' ' . $attribute . '="' . esc_attr($field[$attribute]) . '"';  
             }
         }
 
         // Our definite field class
         $class = $type == 'number' && ! isset($field['class']) ? 'small-text' : $class;
         
-        return '<input class="' . $class . '" id="' . $field['id'] . '" name="' . $field['name']  . '" type="' . $type . '" value="' . $field['values'] . '"' . $placeholder . $attributes . ' />';    
+        return '<input class="' . $class . '" id="' . $field['id'] . '" name="' . $field['name']  . '" type="' . $type . '" value="' . esc_attr($field['values']) . '"' . $placeholder . $attributes . ' />';    
     }
     
     public static function configurations() {
