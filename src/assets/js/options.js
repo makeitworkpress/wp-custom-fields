b59f56e1453696882fa29453f2b82444cdf0b8d5
Michiel <michielsite@hotmail.com>
2019-09-03 12:09:18 +0200

    Header should have a height when scrolling to prevent scrolling clipping with option pages that are the exact window height

--- src/assets/js/options.js
+++ src/assets/js/options.js
@@ -18,9 +18,9 @@ module.exports.init = function(framework) {
 
             if( scrollPosition > 50 ) {
                 scrollHeader.width(scrollWidth);
-                scrollHeader.addClass('wpfc-header-scrolling');
+                scrollHeader.closest('.wp-custom-fields-header').addClass('wpfc-header-scrolling');
             } else {
-                scrollHeader.removeClass('wpfc-header-scrolling');
+                scrollHeader.closest('.wp-custom-fields-header').removeClass('wpfc-header-scrolling');
             }
 
         } );
