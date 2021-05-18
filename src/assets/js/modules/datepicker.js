2affbbe8ae97f26800017026d429709e21b31881
Michiel Tramper <michielsite@hotmail.com>
2021-05-18 11:56:02 +0200

    Improved datepicker display in repeatable groups

--- src/assets/js/modules/datepicker.js
+++ src/assets/js/modules/datepicker.js
@@ -34,4 +34,4 @@ module.exports.init = function(framework) {
 
     }
 
-}
+};
--- src/assets/js/modules/repeatable.js
+++ src/assets/js/modules/repeatable.js
@@ -3,6 +3,7 @@
  * @todo Rewrite this in a more efficient manner.
  */
 var fields = require('./../fields');
+var datepicker = require('./datepicker');
 
 module.exports.init = function(framework) {
 
@@ -31,8 +32,10 @@ module.exports.init = function(framework) {
             
         // Destroy our select2 instances, if it is defined of course
         if( typeof jQuery.fn.select2 !== 'undefined' && jQuery.fn.select2 ) {
-            jQuery('.wpcf-select').select2('destroy');
-        }
+            jQuery(group).find('.wpcf-select').select2('destroy');
+
+            console.log( jQuery(group).find('.wpcf-select') );
+        }     
 
         // Destroy current codemirror instances
         jQuery(framework).find('.wpcf-code-editor-value').each(function (index, node) {
@@ -43,7 +46,21 @@ module.exports.init = function(framework) {
                 codeNodes.push(node);
             }
 
-        });       
+        });
+        
+        // Destroy our datepicker instances before re-adding
+        var datepickers = jQuery(group).find('.wpcf-datepicker');
+
+        if( datepickers.length > 0 ) {
+
+            jQuery(datepickers).each( function() {
+                if( typeof this._flatpickr !== 'undefined' ) {
+                    this._flatpickr.destroy();    
+                }
+            });
+
+        }
+       
 
         // Build our newgroup
         var newGroup = group.clone(true, true);
@@ -51,19 +68,24 @@ module.exports.init = function(framework) {
         // Clone the current group and replace the current keys by new ones. The length is always one bigger as the current array, so it matches the key for the new group.
         newGroup.html(function (i, oldGroup) {
             return oldGroup.replace(/\[\d+\]/g, '[' + length + ']').replace(/\_\d+\_/g, '_' + length + '_');
-        }); 
+        });             
 
         // Empty inputs in our  new group
         newGroup.find('input').val('');
         newGroup.find('textarea').val('');
         newGroup.find('option').attr('selected', false); 
-        newGroup.find('.wpcf-single-media').not('.empty').remove(); // Removes the media from the cloned group   
+        newGroup.find('.wpcf-single-media').not('.empty').remove(); // Removes the media from the cloned group            
                 
         // Finally, insert the newGroup after the current group
-        group.after(newGroup);
+        group.after(newGroup);         
 
-        // Redraw the fields within the group
+        // Redraw all the fields within the group
         fields.init(newGroup);  
+
+        // Reinit old datepicker groups
+        if( datepickers.length > 0 ) {
+            datepicker.init(group);
+        }
         
         // Reinitialize old codemirror groups
         codeNodes.forEach( function(node) {
